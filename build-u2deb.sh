dedc275e9a768fbc272fb874a9f147b956e8e7f1
darealshinji <djcj@gmx.de>
2016-04-24 15:26:31 +0200

    Update URL in u2deb build script

--- build-u2deb.sh
+++ build-u2deb.sh
@@ -5,8 +5,8 @@ set -e
 DIR=`mktemp -d`
 
 cd $DIR
-wget "https://github.com/darealshinji/debian/archive/master.tar.gz"
+wget "https://github.com/darealshinji/debian-packaging/archive/master.tar.gz"
 tar xvf master.tar.gz
 
-cd debian-master/games/unityengine2deb && make PBUILDER=0
+cd debian-packaging-master/games/unityengine2deb && make PBUILDER=0
 
